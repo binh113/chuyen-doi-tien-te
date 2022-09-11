6c75f2423bf6c3ee37bb03ea29719dea9b7a0a69
binh113 <quocbinh230597@gmail.com>
2022-09-11 20:20:58 +0700

    first commit

--- currency-ex.js
+++ currency-ex.js
@@ -3,14 +3,14 @@ function changeMoney() {
     let form = document.getElementById("form").value;
     let to = document.getElementById("to").value;
     let result;
-    if (form ==="dong" && to === "usd") {
-        amount = amount * 23000;
+    if (form === "dong" && to === "usd") {
+        amount = parseInt(amount) * 23000;
         result = "result: " + amount + "$";
     } else if (form === "usd" && to === "dong") {
-        amount = amount / 23000;
+        amount = parseInt(amount) / 23000;
         result = "result: " + amount + "usd";
-    } if (form === to) {
-        result = "result: " + amount + form;
+    } else {
+        result = "result: " + parseInt(amount) + form;
     }
     document.getElementById("result").innerHTML = result
 }
